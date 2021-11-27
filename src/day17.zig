1ceb7716b8770acb06729709a4e7ae1f191118e7
Aiken Cairncross <acairncross@gmail.com>
2021-11-27 21:39:10 +0000

    Day 17 part 2

--- src/day17.zig
+++ src/day17.zig
@@ -1,41 +1,61 @@
 const std = @import("std");
 const total_eggnog: u32 = 150;
 
-fn solve1(container_sizes: []u32, allocator: *std.mem.Allocator) !u32 {
-    var arr = std.ArrayList([total_eggnog + 1]u32).init(allocator);
-    defer arr.deinit();
-    try arr.resize(container_sizes.len + 1);
+fn solve_table(table: []u32, containers: []u32) !void {
+    const table_width = total_eggnog + 1;
+    const table_height = containers.len + 1;
 
     {
-        // 1 way of making 0 eggnog using containers [0..i]
-        var i: usize = 0;
-        while (i < arr.items.len) : (i += 1) {
-            arr.items[i][0] = 1;
+        // 1 way of making 0 eggnog using containers [0..num_containers]
+        var num_containers: usize = 0;
+        while (num_containers < table_height) : (num_containers += 1) {
+            table[num_containers * table_width] = 1;
         }
     }
 
     {
-        // 0 ways of making j>0 eggnog with no containers
-        var j: usize = 1;
-        while (j < arr.items[0].len) : (j += 1) {
-            arr.items[0][j] = 0;
+        // 0 ways of making eggnog_amount>0 eggnog with no containers
+        var eggnog_amount: usize = 1;
+        while (eggnog_amount < table_width) : (eggnog_amount += 1) {
+            table[eggnog_amount] = 0;
         }
     }
 
     {
-        var i: usize = 1;
-        while (i < arr.items.len) : (i += 1) {
-            const this_container_size = container_sizes[i - 1];
-            var j: usize = 1;
-            while (j < arr.items[i].len) : (j += 1) {
-                const ways_without_using_this_container = arr.items[i - 1][j];
-                const ways_using_this_container = if (this_container_size <= j) arr.items[i - 1][j - this_container_size] else 0;
-                arr.items[i][j] = ways_without_using_this_container + ways_using_this_container;
+        var num_containers: usize = 1;
+        while (num_containers < table_height) : (num_containers += 1) {
+            const this_container_size = containers[num_containers - 1];
+            var eggnog_amount: usize = 1;
+            while (eggnog_amount < table_width) : (eggnog_amount += 1) {
+                const ways_without_using_this_container =
+                    table[(num_containers - 1) * table_width + eggnog_amount];
+                const ways_using_this_container =
+                    if (this_container_size <= eggnog_amount)
+                    table[(num_containers - 1) * table_width + (eggnog_amount - this_container_size)]
+                else
+                    0;
+                table[num_containers * table_width + eggnog_amount] =
+                    ways_without_using_this_container + ways_using_this_container;
             }
         }
     }
+}
 
-    return arr.items[container_sizes.len][total_eggnog];
+fn count_ways_by_num_containers(depth: usize, num_containers: usize, eggnog_amount: usize, table: []u32, containers: []u32, ways: *std.AutoHashMap(usize, u32)) void {
+    const table_width = total_eggnog + 1;
+
+    if (table[num_containers * table_width + eggnog_amount] == 0) {
+        return;
+    } else if (eggnog_amount == 0) {
+        return ways.*.put(depth, ways.*.get(depth).? + 1) catch unreachable;
+    }
+
+    const this_container_size = containers[num_containers - 1];
+
+    count_ways_by_num_containers(depth, num_containers - 1, eggnog_amount, table, containers, ways);
+
+    if (this_container_size <= eggnog_amount)
+        count_ways_by_num_containers(depth + 1, num_containers - 1, eggnog_amount - this_container_size, table, containers, ways);
 }
 
 pub fn main() !void {
@@ -48,12 +68,38 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var container_sizes = std.ArrayList(u32).init(&gpa.allocator);
-    defer container_sizes.deinit();
+    var containers = std.ArrayList(u32).init(&gpa.allocator);
+    defer containers.deinit();
 
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        try container_sizes.append(try std.fmt.parseInt(u32, line, 10));
+        try containers.append(try std.fmt.parseInt(u32, line, 10));
     }
 
-    std.debug.print("{d}\n", .{try solve1(container_sizes.items, &gpa.allocator)});
+    var table = std.ArrayList(u32).init(&gpa.allocator);
+    defer table.deinit();
+    try table.resize((total_eggnog + 1) * (containers.items.len + 1));
+    try solve_table(table.items, containers.items);
+
+    std.debug.print("{d}\n", .{table.items[table.items.len - 1]});
+
+    var ways = std.AutoHashMap(usize, u32).init(&gpa.allocator);
+    defer ways.deinit();
+
+    {
+        var i: usize = 0;
+        while (i < containers.items.len + 1) : (i += 1) {
+            try ways.put(i, 0);
+        }
+    }
+    count_ways_by_num_containers(0, containers.items.len, total_eggnog, table.items, containers.items, &ways);
+
+    {
+        var i: usize = 0;
+        while (i < containers.items.len + 1) : (i += 1) {
+            if (ways.get(i).? > 0) {
+                std.debug.print("{d}\n", .{ways.get(i).?});
+                break;
+            }
+        }
+    }
 }
